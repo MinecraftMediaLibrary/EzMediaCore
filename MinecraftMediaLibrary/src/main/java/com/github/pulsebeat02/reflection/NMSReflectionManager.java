8e7500509d93bc5737ee2b77cba9867d9b19b3f7
Brandon Li <40838203+PulseBeat02@users.noreply.github.com>
2021-01-27 22:53:28 -0500

    Fixed Warning in Reflection

--- MinecraftMediaLibrary/src/main/java/com/github/pulsebeat02/reflection/NMSReflectionManager.java
+++ MinecraftMediaLibrary/src/main/java/com/github/pulsebeat02/reflection/NMSReflectionManager.java
@@ -4,6 +4,8 @@ import com.github.pulsebeat02.nms.PacketHandler;
 import com.github.pulsebeat02.logger.Logger;
 import org.bukkit.Bukkit;
 
+import java.lang.reflect.InvocationTargetException;
+
 public class NMSReflectionManager {
 
     public static final String VERSION;
@@ -16,8 +18,8 @@ public class NMSReflectionManager {
         try {
             Logger.info("Loading NMS Class for Version " + VERSION);
             Class<?> clazz = Class.forName("com.github.pulsebeat02.nms.impl" + VERSION + ".NMSMapPacketIntercepter");
-            return (PacketHandler) clazz.newInstance();
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+            return (PacketHandler) clazz.getDeclaredConstructor().newInstance();
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
             Logger.error("The Server Version you are using (" + VERSION + ") is not yet supported by MinecraftMediaLibrary!");
             e.printStackTrace();
             return null;
