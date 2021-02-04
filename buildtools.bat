f4634f57cd687d76dda6d8351d7230b00347456c
Brandon Li <40838203+PulseBeat02@users.noreply.github.com>
2021-02-03 22:55:26 -0500

    Update buildtools.bat

--- buildtools.bat
+++ buildtools.bat
@@ -2,7 +2,6 @@ java -jar BuildTools.jar --rev 1.16.4
 java -jar BuildTools.jar --rev 1.16.3
 java -jar BuildTools.jar --rev 1.16.1
 java -jar BuildTools.jar --rev 1.15.2
-java -jar BuildTools.jar --rev 1.14.4
 "C:\Program Files (x86)\Java\jre1.8.0_191\bin\java.exe" -Xms512m BuildTools.jar --rev 1.14.4
 "C:\Program Files (x86)\Java\jre1.8.0_191\bin\java.exe" -Xms512m -jar BuildTools.jar --rev 1.13.2
 "C:\Program Files (x86)\Java\jre1.8.0_191\bin\java.exe" -Xms512m -jar BuildTools.jar --rev 1.13
