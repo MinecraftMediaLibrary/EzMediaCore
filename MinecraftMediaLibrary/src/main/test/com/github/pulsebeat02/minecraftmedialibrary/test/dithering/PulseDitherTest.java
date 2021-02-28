2e21c6cd9e468cf425d34415d14ba2ed8efbb51c
Brandon Li <40838203+PulseBeat02@users.noreply.github.com>
2021-02-28 01:33:23 -0500

    Yea

---
+++ MinecraftMediaLibrary/src/main/test/com/github/pulsebeat02/minecraftmedialibrary/test/dithering/PulseDitherTest.java
@@ -0,0 +1,11 @@
+package com.github.pulsebeat02.minecraftmedialibrary.test.dithering;
+
+import com.github.pulsebeat02.minecraftmedialibrary.video.dither.PulseDithering;
+
+public class PulseDitherTest {
+
+    public static void main(final String[] args) {
+        new PulseDithering();
+    }
+
+}
