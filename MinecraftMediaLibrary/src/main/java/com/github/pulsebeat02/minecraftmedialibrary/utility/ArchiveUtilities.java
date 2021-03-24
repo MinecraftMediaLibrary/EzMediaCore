7261e92c5f4a832f7f784316bc5ff1fc6d0b0830
Brandon Li <40838203+PulseBeat02@users.noreply.github.com>
2021-03-24 15:10:18 -0400

    Updated Copyright Header

--- MinecraftMediaLibrary/src/main/java/com/github/pulsebeat02/minecraftmedialibrary/utility/ArchiveUtilities.java
+++ MinecraftMediaLibrary/src/main/java/com/github/pulsebeat02/minecraftmedialibrary/utility/ArchiveUtilities.java
@@ -1,5 +1,5 @@
 /*............................................................................................
- . Copyright © 2021 PulseBeat_02                                                             .
+ . Copyright © 2021 Brandon Li                                                               .
  .                                                                                           .
  . Permission is hereby granted, free of charge, to any person obtaining a copy of this      .
  . software and associated documentation files (the “Software”), to deal in the Software     .
