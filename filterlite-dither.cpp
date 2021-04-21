318bd62f1d89f50e21b8e6c670d6b8a7cf43c93f
Brandon Li <40838203+PulseBeat02@users.noreply.github.com>
2021-04-21 15:31:34 -0400

    Fixed Syntax Issues with C++ Code and Uploaded Compiled SO File

--- filterlite-dither.cpp
+++ filterlite-dither.cpp
@@ -4,9 +4,9 @@
 static jint *color_map;
 static jint *full_color_map;
 
-JNIEXPORT void JNICALL Java_com_github_pulsebeat02_minecraftmedialibrary_natives_NativeFilterLiteDither_setup(JNIEnv *env, jobject current, jintArray color, jIntArray full) {
-    color_map = (*env) -> GetIntArrayElements(env, color, NULL);
-    full_color_map = (*env) -> GetIntArrayElements(env, full, NULL);
+JNIEXPORT void JNICALL Java_com_github_pulsebeat02_minecraftmedialibrary_natives_NativeFilterLiteDither_setup(JNIEnv *env, jobject current, jintArray color, jintArray full) {
+    color_map = env -> GetIntArrayElements(color, (jboolean*)true);
+    full_color_map = env -> GetIntArrayElements(full, (jboolean*)true);
 }
 
 char get_best_color(const int rgb) {
@@ -18,22 +18,36 @@ int getBestFullColor(const int red, const int green, const int blue) {
     return full_color_map[red >> 1 << 14 | green >> 1 << 7 | blue >> 1];
 }
 
-
-JNIEXPORT void JNICALL Java_com_github_pulsebeat02_minecraftmedialibrary_natives_NativeFilterLiteDither_dither_native(JNIEnv *env, jobject current, jobject buffer, jintArray arr, jint width) {
-    jint *array = (*env) -> GetIntArrayElements(env, arr, NULL);
-    jsize *height = (*env) -> GetArrayLength(env, jArr);
-    jbyte *data = (*env) -> GetDirectBufferAddress(env, buffer);
+JNIEXPORT void JNICALL Java_com_github_pulsebeat02_minecraftmedialibrary_natives_NativeFilterLiteDither_dither_native(JNIEnv *env, jobject current, jobject buffer, jintArray arr, const jint width) {
+    jint *array = env -> GetIntArrayElements(arr, (jboolean*)true);
+    jsize *height = (jsize*)(size_t)env -> GetArrayLength(arr);
+    jbyte *data = (jbyte*)(env -> GetDirectBufferAddress(buffer));
     const int widthMinus = width - 1;
-    const int heightMinus = height - 1;
+    const int heightMinus = *height - 1;
+
+    /*
+
     const int span = width << 2;
-    int dither_buffer[2][span];
-    for (int y = 0; y < height; ++y) {
+
+    Width is never going to be greater than 1024.
+    This means that 1024 << 2 is equivalent to
+    1024 * 2 * 2 or 4096. We can use this as
+    the max size we can use.
+
+    */
+
+    int** dither_buffer = new int*[2];
+    for (int i = 0; i < 2; ++i) {
+        dither_buffer[i] = new int[4096];
+    }
+
+    for (int y = 0; y < *height; ++y) {
         const bool hasNextY = y < heightMinus;
         const int yIndex = y * width;
         if (!(y & 0x1)) {
             int bufferIndex = 0;
-            int buf1[span] = dither_buffer[0];
-            int buf2[span] = dither_buffer[1];
+            int* buf1 = dither_buffer[0];
+            int* buf2 = dither_buffer[1];
             for (int x = 0; x < width; ++x) {
                 const int index = yIndex + x;
                 const int rgb = array[index];
@@ -62,12 +76,12 @@ JNIEXPORT void JNICALL Java_com_github_pulsebeat02_minecraftmedialibrary_natives
                     buf2[bufferIndex - 2] = delta_g >> 2;
                     buf2[bufferIndex - 1] = delta_b >> 2;
                 }
-                data.put(index, get_best_color(closest));
+                data[index] = get_best_color(closest);
             }
         } else {
             int bufferIndex = width + (width << 1) - 1;
-            int buf1[] = dither_buffer[1];
-            int buf2[] = dither_buffer[0];
+            int* buf1 = dither_buffer[1];
+            int* buf2 = dither_buffer[0];
             for (int x = width - 1; x >= 0; --x) {
                 const int index = yIndex + x;
                 const int rgb = array[index];
@@ -96,8 +110,12 @@ JNIEXPORT void JNICALL Java_com_github_pulsebeat02_minecraftmedialibrary_natives
                     buf2[bufferIndex + 2] = delta_g >> 2;
                     buf2[bufferIndex + 1] = delta_r >> 2;
                 }
-                data.put(index, get_best_color(closest));
+                data[index] = get_best_color(closest);
             }
         }
     }
+    for (int i = 0; i < 2; ++i) {
+        delete[] dither_buffer[i];
+    }
+    delete[] dither_buffer;
 }
