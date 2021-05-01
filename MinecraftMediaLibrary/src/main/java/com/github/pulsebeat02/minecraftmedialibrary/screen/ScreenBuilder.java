9f3710dade1693ea3b02cad57e4985b1f4b38520
Brandon Li <40838203+PulseBeat02@users.noreply.github.com>
2021-05-01 10:42:20 -0400

    Worked on Screen Builder

--- MinecraftMediaLibrary/src/main/java/com/github/pulsebeat02/minecraftmedialibrary/screen/ScreenBuilder.java
+++ MinecraftMediaLibrary/src/main/java/com/github/pulsebeat02/minecraftmedialibrary/screen/ScreenBuilder.java
@@ -3,7 +3,11 @@ package com.github.pulsebeat02.minecraftmedialibrary.screen;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.MapMeta;
+import org.bukkit.util.Vector;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
@@ -16,41 +20,46 @@ public class ScreenBuilder {
     location = player.getLocation();
   }
 
-  /*
-
-  PLAYER LOCATION
-  |
-  |
-  X X X X X ... width
-  X X X X X ...
-  X X X X X ...
-  X X X X X ...
-  X X X X X ...
-  ... ... ...
-  height
-
-  AREA = width * height
-
-  for slot in area
-      row = slot divide width
-      col = slot % width
-      newX = playerLocX +
-
-   */
   public void buildMapScreen(
       @NotNull final Location loc,
       @NotNull final Material mat,
       final int width,
       final int height,
       final int startingMap) {
-    final int maps = width * height;
+
+    final Vector direction = loc.getDirection();
+    final Location center = loc.clone().add(direction);
+
+    final Location rotate = loc.clone();
+    rotate.setPitch(0);
+    rotate.setYaw(loc.getYaw() - 90);
+    final Vector rotation = rotate.getDirection();
+
+    final Location blockLocation = center.clone().add(rotate).subtract(0, width / 2f, 0);
+    rotate.multiply(-1);
+
+    final int initialX = blockLocation.getBlockX();
+    final int initialZ = blockLocation.getBlockZ();
     final World world = Objects.requireNonNull(loc.getWorld());
-    for (int i = 0; i < maps; i++) {
-      final int row = i / width;
-      final int col = i % width;
-      world.getBlockAt(loc.getBlockX() + col, loc.getBlockY() + row, loc.getBlockZ()).setType(mat);
-      // ...
+    for (int x = 0; x < width; x++) {
+      for (int y = 0; y < height; y++) {
+        final int map = startingMap + x * y;
+        final Block block = blockLocation.getBlock();
+        block.setType(mat);
+      }
+      blockLocation.add(0, 1, 0);
+      blockLocation.setX(initialX);
+      blockLocation.setZ(initialZ);
     }
 
+
+  }
+
+  public ItemStack getMapFromID(final int id) {
+    final ItemStack map = new ItemStack(Material.FILLED_MAP);
+    final MapMeta meta = (MapMeta) map.getItemMeta();
+    meta.setMapId(id);
+    map.setItemMeta(meta);
+    return map;
   }
 }
