93a63074c45658a711464fb7ca187970edcb6a7c
Brandon Li <40838203+PulseBeat02@users.noreply.github.com>
2021-04-30 21:51:51 -0400

    Worked on Screen Builder

--- MinecraftMediaLibrary/src/main/java/com/github/pulsebeat02/minecraftmedialibrary/screen/ScreenBuilder.java
+++ MinecraftMediaLibrary/src/main/java/com/github/pulsebeat02/minecraftmedialibrary/screen/ScreenBuilder.java
@@ -1,23 +1,56 @@
 package com.github.pulsebeat02.minecraftmedialibrary.screen;
 
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Objects;
+
 public class ScreenBuilder {
 
-    private final Location location;
+  private final Location location;
 
-    public ScreenBuilder(@NotNull final Player player) {
-        location = player.getLocation();
+  public ScreenBuilder(@NotNull final Player player) {
+    location = player.getLocation();
+  }
+
+  /*
+
+  PLAYER LOCATION
+  |
+  |
+  X X X X X ... width
+  X X X X X ...
+  X X X X X ...
+  X X X X X ...
+  X X X X X ...
+  ... ... ...
+  height
+
+  AREA = width * height
+
+  for slot in area
+      row = slot divide width
+      col = slot % width
+      newX = playerLocX +
+
+   */
+  public void buildMapScreen(
+      @NotNull final Location loc,
+      @NotNull final Material mat,
+      final int width,
+      final int height,
+      final int startingMap) {
+    final int maps = width * height;
+    final World world = Objects.requireNonNull(loc.getWorld());
+    for (int i = 0; i < maps; i++) {
+      final int row = i / width;
+      final int col = i % width;
+      world.getBlockAt(loc.getBlockX() + col, loc.getBlockY() + row, loc.getBlockZ()).setType(mat);
+      // ...
     }
 
-    // TODO: Implement map build helper class
-    public void buildMapScreen(final int width, final int height, final int startingMap) {
-        final int maps = width * height;
-        for (int i = 0; i < maps; i++) {
-            final int row = i % width;
-        }
-    }
-
+  }
 }
