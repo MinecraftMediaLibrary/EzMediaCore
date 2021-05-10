b65d12331ff0c914990349dfc87e651f8808adbd
Brandon Li <40838203+PulseBeat02@users.noreply.github.com>
2021-05-10 08:28:23 -0400

    Updated buildtools.bat

--- buildtools.bat
+++ buildtools.bat
@@ -1,16 +1,46 @@
-                                                            java -jar BuildTools.jar --rev 1.16.4
-                                                            java -jar BuildTools.jar --rev 1.16.3
-                                                            java -jar BuildTools.jar --rev 1.16.1
-                                                            java -jar BuildTools.jar --rev 1.15.2
-"C:\Program Files (x86)\Java\jre1.8.0_191\bin\java.exe" -Xms512m -jar BuildTools.jar --rev 1.14.4
-"C:\Program Files (x86)\Java\jre1.8.0_191\bin\java.exe" -Xms512m -jar BuildTools.jar --rev 1.13.2
-"C:\Program Files (x86)\Java\jre1.8.0_191\bin\java.exe" -Xms512m -jar BuildTools.jar --rev 1.13
-"C:\Program Files (x86)\Java\jre1.8.0_191\bin\java.exe" -Xms512m -jar BuildTools.jar --rev 1.12.2
-"C:\Program Files (x86)\Java\jre1.8.0_191\bin\java.exe" -Xms512m -jar BuildTools.jar --rev 1.11.2
-"C:\Program Files (x86)\Java\jre1.8.0_191\bin\java.exe" -Xms512m -jar BuildTools.jar --rev 1.10.2
-"C:\Program Files (x86)\Java\jre1.8.0_191\bin\java.exe" -Xms512m -jar BuildTools.jar --rev 1.9.4
-"C:\Program Files (x86)\Java\jre1.8.0_191\bin\java.exe" -Xms512m -jar BuildTools.jar --rev 1.9.2
-"C:\Program Files (x86)\Java\jre1.8.0_191\bin\java.exe" -Xms512m -jar BuildTools.jar --rev 1.8.8
-"C:\Program Files (x86)\Java\jre1.8.0_191\bin\java.exe" -Xms512m -jar BuildTools.jar --rev 1.8.3
-"C:\Program Files (x86)\Java\jre1.8.0_191\bin\java.exe" -Xms512m -jar BuildTools.jar --rev 1.8
+ECHO Running BuildTools 1.16.4
+java -Xms512m -jar BuildTools.jar --rev 1.16.4
+
+ECHO Running BuildTools 1.16.3
+java -Xms512m -jar BuildTools.jar --rev 1.16.3
+
+ECHO Running BuildTools 1.16.1
+java -Xms512m -jar BuildTools.jar --rev 1.16.1
+
+ECHO Running BuildTools 1.15.2
+java -Xms512m -jar BuildTools.jar --rev 1.15.2
+
+ECHO Running BuildTools 1.14.4
+java -Xms512m -jar BuildTools.jar --rev 1.14.4
+
+ECHO Running BuildTools 1.13.2
+java -Xms512m -jar BuildTools.jar --rev 1.13.2
+
+ECHO Running BuildTools 1.13
+java -Xms512m -jar BuildTools.jar --rev 1.13
+
+ECHO Running BuildTools 1.12.2
+java -Xms512m -jar BuildTools.jar --rev 1.12.2
+
+ECHO Running BuildTools 1.11.2
+java -Xms512m -jar BuildTools.jar --rev 1.11.2
+
+ECHO Running BuildTools 1.10.2
+java -Xms512m -jar BuildTools.jar --rev 1.10.2
+
+ECHO Running BuildTools 1.9.4
+java -Xms512m -jar BuildTools.jar --rev 1.9.4
+
+ECHO Running BuildTools 1.9.2
+java -Xms512m -jar BuildTools.jar --rev 1.9.2
+
+ECHO Running BuildTools 1.8.8
+java -Xms512m -jar BuildTools.jar --rev 1.8.8
+
+ECHO Running BuildTools 1.8.3
+java -Xms512m -jar BuildTools.jar --rev 1.8.3
+
+ECHO Running BuildTools 1.8
+java -Xms512m -jar BuildTools.jar --rev 1.8
+
 PAUSE
