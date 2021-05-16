7a2e66e1af2ca92bb42b4177204e921b8ce3c53c
Brandon Li <40838203+PulseBeat02@users.noreply.github.com>
2021-05-16 10:11:35 -0400

    Solved Gradle Relocation Issue Temporarily

--- minecraftmedialibrary-nms/build.gradle.kts
+++ minecraftmedialibrary-nms/build.gradle.kts
@@ -1,5 +1,9 @@
 description = "minecraftmedialibrary-nms"
 
+plugins {
+    id("com.github.johnrengelman.shadow") version "7.0.0"
+}
+
 dependencies {
     implementation(project(":v1_16_R3"))
     implementation(project(":v1_16_R2"))
