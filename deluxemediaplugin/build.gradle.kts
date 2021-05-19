9339449b5f4f5f87bbcbd7f6816b5d0881922b67
Brandon Li <40838203+PulseBeat02@users.noreply.github.com>
2021-05-19 14:59:38 -0400

    Temporarily Fixed Plugin

--- deluxemediaplugin/build.gradle.kts
+++ deluxemediaplugin/build.gradle.kts
@@ -21,4 +21,17 @@ tasks.withType<ShadowJar> {
     relocate("net.kyori", "com.github.pulsebeat02.deluxemediaplugin.lib.kyori")
     relocate("org.bstats", "com.github.pulsebeat02.deluxemediaplugin.lib.bstats")
     relocate("com.mojang.brigadier", "com.github.pulsebeat02.deluxemediaplugin.lib.brigadier")
+    relocate("uk.co.caprica.vlcj", "com.github.pulsebeat02.vlcj")
+    relocate("uk.co.caprica.vlcj.binding", "com.github.pulsebeat02.vlcj.binding")
+    relocate("uk.co.caprica.nativestreams", "com.github.pulsebeat02.vlcj.nativestreams")
+    relocate("com.github.kiulian.downloader", "com.github.pulsebeat02.youtube")
+    relocate("ws.schild.jave", "com.github.pulsebeat02.jave")
+    relocate("org.apache.commons.compress", "com.github.pulsebeat02.compress")
+    relocate("org.rauschig.jarchivelib", "com.github.pulsebeat02.jarchivelib")
+    relocate("org.tukaani.xz", "com.github.pulsebeat02.xz")
+    relocate("org.ow2.asm", "com.github.pulsebeat02.asm")
+    relocate("org.ow2.asm.commons", "com.github.pulsebeat02.asm.commons")
+    relocate("org.apache.commons.io", "org.bukkit.craftbukkit.libs.org.apache.commons.io")
+    relocate("com.wrapper.spotify", "com.github.pulsebeat02.spotify")
 }
+
