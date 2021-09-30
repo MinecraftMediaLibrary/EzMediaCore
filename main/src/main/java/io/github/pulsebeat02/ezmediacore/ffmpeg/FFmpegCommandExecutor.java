43ce19116715400b3f16852cfe01dd6a4088b84f
PulseBeat02 <brandonli2006ma@gmail.com>
2021-09-30 17:18:28 -0400

    ✨ General Logger Improvements

--- main/src/main/java/io/github/pulsebeat02/ezmediacore/ffmpeg/FFmpegCommandExecutor.java
+++ main/src/main/java/io/github/pulsebeat02/ezmediacore/ffmpeg/FFmpegCommandExecutor.java
@@ -124,6 +124,7 @@ public class FFmpegCommandExecutor implements FFmpegArgumentPreparation {
   @Override
   public void executeWithLogging(@Nullable final Consumer<String> logger) {
     this.onBeforeExecution();
+    final boolean streamerLog = this instanceof FFmpegMediaStreamer;
     if (!this.cancelled) {
       final boolean consume = logger != null;
       final ProcessBuilder builder = new ProcessBuilder(this.arguments);
@@ -141,7 +142,11 @@ public class FFmpegCommandExecutor implements FFmpegArgumentPreparation {
             if (consume) {
               logger.accept(line);
             }
-            Logger.directPrintFFmpeg(line);
+            if (streamerLog) {
+              Logger.directPrintFFmpegStream(line);
+            } else {
+              Logger.directPrintFFmpegPlayer(line);
+            }
           }
         }
       } catch (final IOException e) {
