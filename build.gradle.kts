03cca4c73151cd00b520e6d68ee2464f10d33d10
Brandon Li <40838203+PulseBeat02@users.noreply.github.com>
2021-12-30 14:27:02 -0500

    Added Java 17 Gradle Tool Chain

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,8 +21,9 @@ subprojects {
     }
 
     java {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(17))
+        }
     }
 
     tasks {
