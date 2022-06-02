35e9f062a007700f6ed3fed378e44a72eb765b6e
Brandon Li <40838203+PulseBeat02@users.noreply.github.com>
2022-06-02 16:26:41 -0400

    Updated settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,11 +4,10 @@ include("api", "v1_18_R2", "main", "lib", "deluxemediaplugin")
 
 mapOf(
     "api" to "ezmediacore-api",
-    "v1_18_R1" to "v1_18_R1",
+    "v1_18_R2" to "v1_18_R2",
     "main" to "ezmediacore",
     "lib" to "ezmediacore-lib",
     "deluxemediaplugin" to "deluxemediaplugin"
 ).forEach {
     findProject(it.key)?.name = it.value
 }
-include("v1_18_R1")
