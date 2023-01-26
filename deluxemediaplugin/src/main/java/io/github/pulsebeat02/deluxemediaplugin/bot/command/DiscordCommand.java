5608175710895dda1055117b464470d0225cba61
Brandon Li <40838203+PulseBeat02@users.noreply.github.com>
2023-01-26 10:56:19 -0500

    Updated Year License

--- deluxemediaplugin/src/main/java/io/github/pulsebeat02/deluxemediaplugin/bot/command/DiscordCommand.java
+++ deluxemediaplugin/src/main/java/io/github/pulsebeat02/deluxemediaplugin/bot/command/DiscordCommand.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2021 Brandon Li
+ * Copyright (c) 2023 Brandon Li
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -21,7 +21,6 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-
 package io.github.pulsebeat02.deluxemediaplugin.bot.command;
 
 import io.github.pulsebeat02.deluxemediaplugin.bot.MediaBot;
